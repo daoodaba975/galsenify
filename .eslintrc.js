75b7282287e024dc951d1d4370f98d981334072c
daoodaba975 <davinhoba9@outlook.fr>
2025-05-30 23:00:25 +0000

    [BUILD]: Update ESLint and Prettier configurations; refine .gitignore and .npmignore 🛠

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,9 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-  ],
+  extends: ['plugin:@typescript-eslint/recommended'],
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
   },
-  rules: {
-  },
+  rules: {},
 };
